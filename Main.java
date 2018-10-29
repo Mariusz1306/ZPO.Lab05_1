ae57a91c29f51793ca8a31e97f4a7ae5306646e6
Mariusz1306 <33585033+Mariusz1306@users.noreply.github.com>
2018-10-29 09:58:16 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,77 +1,80 @@
-
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
 import java.util.stream.Stream;
 
 public class Main {
 
-    //Interfes potrzebny do lambdy
     interface Person {
         int processPerson(String str1, String str2);
     }
 
-    public static void main(String[] args){
-        //Pierwszy ArrayList
+    public static void main(String[] args) {
+
         ArrayList<String> people = new ArrayList();
 
-        //Drugi ArrayList, lepiej się na nim pracuje
         ArrayList<String[]> peopleSplit = new ArrayList();
+
+        LinkedHashMap<String, Integer> nationalities = new LinkedHashMap<>(4);
 
         int max1 = 0, max2 = 0;
         int maxIndex1 = 0, maxIndex2 = 0;
         int min1 = 0, min2 = 0;
         int minIndex1 = 0, minIndex2 = 0;
 
-        //Metoda lambda
-        Person suma = (str1, str2) -> {
-            return (Integer.parseInt(str1) + Integer.parseInt(str2));
-        };
+        Person suma = (str1, str2) -> (Integer.parseInt(str1) + Integer.parseInt(str2));
 
-        String file = "C://Money.txt"; //Pełna ścieżka do pliku
-        try (Stream<String> stream = Files.lines(Paths.get(file))) {
-            stream.peek(e -> System.out.println("Value: " + e)) //Podejrzenie strumienia
-            .forEach(people::add); //dodajemy do pierwszej ArrayListy
+        String file = "C://Users//194930//IdeaProjects//ZPO.Lab05_1//dane.txt";
+        try (Stream<String> stream = Files.lines(Paths.get(file), StandardCharsets.ISO_8859_1)) {
+            //stream.peek(e -> System.out.println("Value: " + e)).forEach(people::add);
+            //stream.peek(e -> System.out.println(e.split(" "))).forEach(people::add);
+            stream.peek(e -> {
+                String[] test = e.split(" ");
+                peopleSplit.add(test);
+                System.out.println(e);
+            }).count();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        //Dodaj elementy z pierwszej ArrayListy, do drugiej ArrayListy (tej lepszej)
-        for (int i=0; i<people.size();i++){
-            peopleSplit.add(people.get(i).split(" "));
-        }
-
-        //Zapisz indexy najlepiej zarabiajacych polaków
-        for (int i=0; i<peopleSplit.size();i++){
-            if (peopleSplit.get(i)[2].equalsIgnoreCase("PL")){
-                if (Integer.parseInt(peopleSplit.get(i)[3]) > max1){
+        for (int i = 0; i < peopleSplit.size(); i++) {
+            if (nationalities.containsKey(peopleSplit.get(i)[2])) {
+                nationalities.replace(peopleSplit.get(i)[2], (nationalities.get(peopleSplit.get(i)[2]) + 1));
+            } else {
+                nationalities.put(peopleSplit.get(i)[2], 1);
+            }
+            if (peopleSplit.get(i)[2].equalsIgnoreCase("PL")) {
+                if (Integer.parseInt(peopleSplit.get(i)[3]) > max1) {
                     max2 = max1;
                     maxIndex2 = maxIndex1;
                     max1 = Integer.parseInt(peopleSplit.get(i)[3]);
                     maxIndex1 = i;
-                } else if (Integer.parseInt(peopleSplit.get(i)[3]) > max2){
+                } else if (Integer.parseInt(peopleSplit.get(i)[3]) > max2) {
                     max2 = Integer.parseInt(peopleSplit.get(i)[3]);
                     maxIndex2 = i;
                 }
             }
         }
 
-        //Przyjmij, że najlepiej zarabiający, są jednoczesnie najgorzej zarabiającymy
         min1 = max2;
         minIndex1 = maxIndex2;
         min2 = max1;
         minIndex2 = maxIndex1;
 
-        //Zapisz indexy najgorzej zarabiajacych polaków
-        for (int i=0; i<peopleSplit.size();i++){
-            if (peopleSplit.get(i)[2].equalsIgnoreCase("PL")){
-                if (Integer.parseInt(peopleSplit.get(i)[3]) < min1){
+        for (int i = 0; i < peopleSplit.size(); i++) {
+            if (peopleSplit.get(i)[2].equalsIgnoreCase("PL")) {
+                if (Integer.parseInt(peopleSplit.get(i)[3]) < min1) {
                     min2 = min1;
                     minIndex2 = minIndex1;
                     min1 = Integer.parseInt(peopleSplit.get(i)[3]);
                     minIndex1 = i;
-                } else if (Integer.parseInt(peopleSplit.get(i)[3]) < min2){
+                } else if (Integer.parseInt(peopleSplit.get(i)[3]) < min2) {
                     min2 = Integer.parseInt(peopleSplit.get(i)[3]);
                     minIndex2 = i;
                 }
@@ -79,12 +82,13 @@ public class Main {
         }
 
         System.out.print("\nSuma zarobków najwięcej zarabiających polaków: ");
-        //Uzyj tej naszej lambdy
         System.out.println(suma.processPerson(
                 peopleSplit.get(maxIndex1)[3], peopleSplit.get(maxIndex2)[3]));
         System.out.print("\nSuma zarobków najmnniej zarabiających polaków: ");
-        //Uzyj tej naszej lambdy x2
         System.out.println(suma.processPerson(
                 peopleSplit.get(minIndex1)[3], peopleSplit.get(minIndex2)[3]));
+
+        for (String key : nationalities.keySet())
+            System.out.println(key + " " + nationalities.get(key));
     }
 }
